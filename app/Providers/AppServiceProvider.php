6de8820119e69f09c933f210610403025c0f8ad2
Santiago Ariel <santiagoarielv98@gmail.com>
2025-02-26 15:54:07 -0300

    Add PanelSwitch configuration to AppServiceProvider and update composer dependencies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use BezhanSalleh\PanelSwitch\PanelSwitch;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        PanelSwitch::configureUsing(function (PanelSwitch $panelSwitch) {
+            $panelSwitch
+                ->visible(fn (): bool => auth()->user()?->hasAnyRole([
+                    'super_admin',
+                ]));
+        });
     }
 }
