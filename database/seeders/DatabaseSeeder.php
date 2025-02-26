a16e5cb6bffc57d8d69e8c2ae33226b70e824329
Santiago Ariel <santiagoarielv98@gmail.com>
2025-02-25 22:26:16 -0300

    Add models and migrations for countries, states, and cities; update composer dependencies and seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,13 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Demo User',
+            'email' => 'demo@example.com',
         ]);
+
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+        ]);  
     }
 }
