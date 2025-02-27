b3ef9add5a435893666e8815bc38973e6d9c0df9
Santiago Ariel <santiagoarielv98@gmail.com>
2025-02-27 19:42:29 -0300

    Uncomment name field in User model and migration to restore user name functionality

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,7 +27,7 @@ class User extends Authenticatable implements FilamentUser
      * @var list<string>
      */
     protected $fillable = [
-        // 'name',
+        'name',
         'email',
         'password',
         'country_id',
